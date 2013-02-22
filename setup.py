498ebf5d8aab8810b1f3225915ca91459874ac4f
Stephan Richter <stephan.richter@gmail.com>
2013-02-22 17:27:53 -0500

    Back to development: 4.1.0a2

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def alltests():
 
 setup(
     name='zope.browserpage',
-    version='4.1.0a1',
+    version='4.1.0a2.dev0',
     url='http://pypi.python.org/pypi/zope.browserpage/',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
