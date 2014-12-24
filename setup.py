46ac17fbd7e018e9366b50ba2df911833c6d4e11
Tres Seaver <tseaver@palladion.com>
2014-12-24 11:18:49 -0500

    svb

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ def alltests():
 
 setup(
     name='zope.browserpage',
-    version='4.1.0',
+    version='4.1.1.dev0',
     url='http://pypi.python.org/pypi/zope.browserpage/',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
