a74d0938b5d0c1d03ecd19c144e6092638570d5b
Jason Madden <jamadden@gmail.com>
2017-08-02 13:12:01 -0500

    Preparing release 4.2.0

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ TESTS_REQUIRE = [
 
 setup(
     name='zope.browserpage',
-    version='4.2.0.dev0',
+    version='4.2.0',
     url='https://github.com/zopefoundation/zope.browserpage',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
