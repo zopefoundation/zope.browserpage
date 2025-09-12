c71bcd592c426096ea7bca790a17833a0e0529e6
Michael Howitz <icemac@gmx.net>
2025-09-12 08:56:57 +0200

    Back to development: 6.1

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ TESTS_REQUIRE = [
 
 setup(
     name='zope.browserpage',
-    version='6.0',
+    version='6.1.dev0',
     url='https://github.com/zopefoundation/zope.browserpage',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.dev',
